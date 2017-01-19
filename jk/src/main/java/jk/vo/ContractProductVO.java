8fd0a18581820126176fd6c3acf28beb85b8cd3b
tyler <zhang_xiaotai@icloud.com>
2017-01-19 19:20:01 +0800

    关联查询

---
+++ jk/src/main/java/jk/vo/ContractProductVO.java
@@ -0,0 +1,171 @@
+package jk.vo;
+
+import java.util.List;
+
+import jk.domain.Contract;
+import jk.domain.ExtCproduct;
+import jk.domain.Factory;
+
+/**
+ * @author zhangtai
+ *
+ */
+public class ContractProductVO {
+
+	private String id;
+
+	private Contract contract;
+	private List<ExtCproduct> extCproducts;
+	private Factory factory;// 多对一关系
+
+	private String productNo;
+	private String productImage;
+	private String productDesc;
+	private Integer cnumber;
+	private Integer outNumber;
+	private String loadingRate;
+	private Integer boxNum;
+	private String packingUnit;
+	private Double price;
+	private Double amount;
+	private Integer finished;
+	private String exts;
+	private Integer orderNo;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getProductNo() {
+		return productNo;
+	}
+
+	public void setProductNo(String productNo) {
+		this.productNo = productNo;
+	}
+
+	public String getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+
+	public String getProductDesc() {
+		return productDesc;
+	}
+
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+
+	public Integer getCnumber() {
+		return cnumber;
+	}
+
+	public void setCnumber(Integer cnumber) {
+		this.cnumber = cnumber;
+	}
+
+	public Integer getOutNumber() {
+		return outNumber;
+	}
+
+	public void setOutNumber(Integer outNumber) {
+		this.outNumber = outNumber;
+	}
+
+	public String getLoadingRate() {
+		return loadingRate;
+	}
+
+	public void setLoadingRate(String loadingRate) {
+		this.loadingRate = loadingRate;
+	}
+
+	public Integer getBoxNum() {
+		return boxNum;
+	}
+
+	public void setBoxNum(Integer boxNum) {
+		this.boxNum = boxNum;
+	}
+
+	public String getPackingUnit() {
+		return packingUnit;
+	}
+
+	public void setPackingUnit(String packingUnit) {
+		this.packingUnit = packingUnit;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Integer getFinished() {
+		return finished;
+	}
+
+	public void setFinished(Integer finished) {
+		this.finished = finished;
+	}
+
+	public String getExts() {
+		return exts;
+	}
+
+	public void setExts(String exts) {
+		this.exts = exts;
+	}
+
+	public Integer getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(Integer orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public Contract getContract() {
+		return contract;
+	}
+
+	public void setContract(Contract contract) {
+		this.contract = contract;
+	}
+
+	public List<ExtCproduct> getExtCproducts() {
+		return extCproducts;
+	}
+
+	public void setExtCproducts(List<ExtCproduct> extCproducts) {
+		this.extCproducts = extCproducts;
+	}
+
+	public Factory getFactory() {
+		return factory;
+	}
+
+	public void setFactory(Factory factory) {
+		this.factory = factory;
+	}
+
+}
